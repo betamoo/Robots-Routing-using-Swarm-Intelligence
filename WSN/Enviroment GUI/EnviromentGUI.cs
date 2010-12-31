50d03dbc98d2ff49667ff655c1838eeead4ceccc
ahmed_ezzat_cp <SND\ahmed_ezzat_cp>
2010-12-31 21:34:06 +0000

    UI Components Added , implementation should begin now

--- WSN/Enviroment GUI/EnviromentGUI.cs
+++ WSN/Enviroment GUI/EnviromentGUI.cs
@@ -9,9 +9,9 @@ using System.Windows.Forms;
 
 namespace Enviroment_GUI
 {
-    public partial class Form1 : Form
+    public partial class EnviromentGUI : Form
     {
-        public Form1()
+        public EnviromentGUI()
         {
             InitializeComponent();
         }
